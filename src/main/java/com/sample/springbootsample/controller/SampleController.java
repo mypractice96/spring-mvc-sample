d6e06d3a769cd247929fd311cfea9849a1146678
mypractice96 <53252202+mypractice96@users.noreply.github.com>
2020-10-11 21:51:35 +0530

    Update SampleController.java

--- src/main/java/com/sample/springbootsample/controller/SampleController.java
+++ src/main/java/com/sample/springbootsample/controller/SampleController.java
@@ -6,13 +6,14 @@ import java.net.UnknownHostException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SampleController {
 
 
 	
-	@RequestMapping(value = {"","/","/home"})
+	@RequestMapping(path = {"","/","/home"},method=RequestMethod.GET)
 	public String showHomePage(ModelMap model) {
 				
 		try {
