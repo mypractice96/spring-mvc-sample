5d8f09571c453ac24258adaf5ca2c5478be721cb
mypractice96 <53252202+mypractice96@users.noreply.github.com>
2021-09-14 02:08:49 -0400

    Create SampleControllerTest.java

---
+++ src/test/java/com/sample/springbootsample/controller/SampleControllerTest.java
@@ -0,0 +1,17 @@
+package com.sample.springbootsample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleControllerTests {
+
+	@Test
+	void testAddition1() {
+	}
+  
+  @Test
+	void testAddition2() {
+	}
+
+}
